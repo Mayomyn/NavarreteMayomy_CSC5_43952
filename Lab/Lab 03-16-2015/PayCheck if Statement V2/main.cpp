c54ba28c5f61bc153fcbc7d3d1df9a7867d6f6f4
mayomyn <mayomyn@rocketmail.com>
2015-03-16 09:05:25 -0700

    Signed-off-by: mayomyn <mayomyn@rocketmail.com>

---
+++ Lab/Lab 03-16-2015/PayCheck if Statement V2/main.cpp
@@ -0,0 +1,52 @@
+/* 
+ * File:   main.cpp
+ * Author: Mayomy Navarrete
+ *Created on March 16, 2015, 8:11 AM
+ * 
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+// Declare Variables
+    ofstream out;//output the results in file
+    float hrsWkd;//Hours worked (hrs)
+    float payRate;//Pay Rate (dollars/hour)
+    float oRate=1.5f;//multiplicative factor of payrate
+    float oTime=40.0f;//where overtime begins (hours)
+    float payChk;//gross pay (dollars)
+    //open the file
+    out.open("Payroll.dat");
+    //prompt the ser for inputs
+    cout<<"How many hours this week did you work?"<<endl;
+    cin>>hrsWkd;
+    cout<<"What is your pay rate ($'s/hr)"<<endl;
+    cin>>payRate;
+    //Calculate the paycheck
+    if(hrsWkd<=Time){
+        payRate*(hrsWkd=(oRate-1)*(hrsWkd-oTime));
+    }
+    //output the results to the screen
+    cout<<"Hours worked = "<<hrsWkd<<"(hrs)"<<endl;
+    cout<<"Pay Rate = $"<<payRate<<"/(hr)"<<endl;
+    cout<<"Overtime rate = "<<oRate<<endl;
+    cout<<"Overtime begins at "<<oTime<<"(hrs"<<endl;
+    cout<<"Gross pa = $"<<payChk<<endl;
+    //output the results to the file
+    out<<"Hours worked = "<<hrsWkd<<"(hrs)"<<endl;
+    out<<"Pay Rate = $"<<payRate<<"/(hr)"<<endl;
+    out<<"Overtime rate = "<<oRate<<endl;
+    out<<"Overtime begins at "<<oTime<<"(hrs"<<endl;
+    out<<"Gross pa = $"<<payChk<<endl;
+    out.close();
+    //close the file
+   
+    return 0;
+}
